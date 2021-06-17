a25c8f2f129bf82ec5ccf7c0df949ca997bf55d8
Creator125OCA <85368990+Creator125@users.noreply.github.com>
2021-06-17 20:16:04 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,27 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
+
+class cubo{//Abiendo la clase Cubo
+//Atributos
+  string material;
+  int tamanño;
+  string color;
+
+  public static void Rotar(){//Abriendo el metodo de Rotar
+    //Acciones
+    int rotacion;
+    return rotacion;
+  }//Cerrando el metodo de Rotar
+
+  public static void Mover(){//Abiendo el metodo Mover
+//Acciones
+  int desplazamiento;
+  return desplazamiento;
+  }//Cerrando el metodo Mover
+
+}
